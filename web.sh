fdb9103344b805f8a8fd4d89040110ff03515306
RaHuL-RaNa1 <124451661+RaHuL-RaNa1@users.noreply.github.com>
2024-06-19 21:51:19 +0530

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-
-
-sudo yum install httpd unzip wget -y
-sudo systemctl enable --now httpd
-
-sudo wget $LINK
-sudo unzip *.zip
-sudo rm -rf *.zip
-sudo mv * html
-
-sudo rm -rf /var/www/html
-sudo mv html /var/www/
-
-sudo systemctl restart httpd
-sudo systemctl enable --now httpd
-
+echo "hello $TEST"
