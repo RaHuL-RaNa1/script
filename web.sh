16e072ba242afa78dc537357a4db16442d637ace
RaHuL-RaNa1 <124451661+RaHuL-RaNa1@users.noreply.github.com>
2024-06-19 11:54:53 +0530

    Update web.sh

--- web.sh
+++ web.sh
@@ -3,13 +3,22 @@
 
 yum install httpd unzip wget -y
 systemctl enable --now httpd
+
+# Directory bnali
 mkdir -p /home/script
 cd /home/script
+rm -rf *
+# Download and unzip the file
+
 wget $LINK
-unzip *
+unzip *.zip
 rm -rf *.zip
-mkdir $new_dir
-mv * $new_dir
+mkdir -p $new_dir
+# Move the contents to the specified directory
+mv * html
+cd html
+mv * $new_dir/
+# create the configuration file
 
 cd /etc/httpd/conf.d/
 echo "Listen $new_port
@@ -25,4 +34,6 @@ servername $IP
 </Directory>"  >rahul.conf
 
 systemctl restart httpd
+systemctl enable --now httpd
+
 awk '/Listen/{print}' /etc/httpd/conf.d/rahul.conf
