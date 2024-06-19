156eb57004dc4c2ae5c9f4925b9e9122929a4a8e
RaHuL-RaNa1 <124451661+RaHuL-RaNa1@users.noreply.github.com>
2024-06-19 22:00:30 +0530

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 echo "hello $TEST"
 wget $TEST
+
+sudo yum install httpd wget unzip -y
+sudo systemctl enable --now httpd
+
+unzip *.zip
+rm -rf *.zip
+sudo rm -rf /var/www/html
+sudo mv * html
+mv html /var/www/
+
