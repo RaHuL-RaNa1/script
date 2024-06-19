69e8102936ad92c9dedecbaa07096bfe818e34b7
RaHuL-RaNa1 <124451661+RaHuL-RaNa1@users.noreply.github.com>
2024-06-19 22:10:42 +0530

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo "hello $TEST"
-wget $TEST
+
 
 sudo yum install httpd wget unzip -y
 sudo systemctl enable --now httpd
-
+sudo mkdir /var/lib/jenkins/workspace/job3/abc
+sudo cd /var/lib/jenkins/workspace/job3/abc
+wget $TEST
 unzip *.zip
 rm -rf *.zip
 sudo rm -rf /var/www/html
-sudo mv oxer-html html
+sudo mv * html
 sudo mv html /var/www/
 
