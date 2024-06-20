7a52f7e7061b5931a912e4064660591b0398b907
RaHuL-RaNa1 <124451661+RaHuL-RaNa1@users.noreply.github.com>
2024-06-20 16:09:37 +0530

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
-echo "hello $TEST"
 
-
-sudo yum install httpd wget unzip -y
+sudo yum install httpd unzip wget -y
 sudo systemctl enable --now httpd
-sudo mkdir /var/lib/jenkins/workspace/jenkins1/abc
-cd abc
-wget $TEST
-unzip *.zip
-rm -rf *.zip
-sudo rm -rf /var/www/html
-sudo mkdir html
+
+sudo mkdir -p /var/lib/jenkins/workspace/jenkins1/abc
+sudo cd /var/lib/jenkins/workspace/jenkins1/abc
+sudo rm -rf *
+# Download and unzip the file
+sudo wget $LINK
+sudo unzip *.zip
+sudo rm -rf *.zip
+
+sudo mkdir -p $new_dir
+mkdir html
 sudo mv * html
-sudo mv html /var/www/
+sudo cd html
+sudo mv * $new_dir/
+ 
+ # create the configuration file
+ cd /etc/httpd/conf.d/
+ echo "Listen $new_port
+ <virtualhost *:$new_port>
+ documentroot $new_dir
+ servername $IP
+ </virtualhost>
+
+ <Directory "$new_dir">
+     AllowOverride None
+     Require all granted
+ </Directory>"  >rahul.conf
+# Restart Apache to apply the changes
+sudo systemctl restart httpd
+sudo systemctl enable --now httpd
 
