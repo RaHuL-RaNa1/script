572a4b5d9961a8e5c4a66b650c173ea92f377d5c
RaHuL-RaNa1 <124451661+RaHuL-RaNa1@users.noreply.github.com>
2024-06-20 16:14:12 +0530

    Update web.sh

--- web.sh
+++ web.sh
@@ -3,8 +3,8 @@
 sudo yum install httpd unzip wget -y
 sudo systemctl enable --now httpd
 
-sudo mkdir -p /var/lib/jenkins/workspace/jenkins1/abc
-sudo cd /var/lib/jenkins/workspace/jenkins1/abc
+sudo mkdir -p /var/lib/jenkins/workspace/prac/abc
+sudo cd //var/lib/jenkins/workspace/prac/abc
 sudo rm -rf *
 # Download and unzip the file
 sudo wget $LINK
